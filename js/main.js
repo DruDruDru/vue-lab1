e695d86d9447f3cfda9b0385f8a22047f9e5c40a
DruDruDru <borisrasgard251@gmail.com>
2024-01-15 22:25:49 +0700

    Theme 10 User Events

--- js/main.js
+++ js/main.js
@@ -2,37 +2,38 @@ Vue.component('product', {
     props: {
         premium: {
             type: Boolean,
-            required: true,
+            required: true
         }
     },
     template: `
         <div class="product">
-            <div class="product-image">
-                <img :src="image" :alt="altText"/>
-            </div>
-            <div class="product-info">
-                <h1>{{ title }}</h1>
-                <p v-if="inStock">In stock</p>
-                <p v-else>Out of Stock</p>
-                <p>Shipping {{ shipping }}</p>
-                <div
-                class="color-box"
-                v-for="(variant, index) in variants"
-                :key="variant.variantId"
-                :style="{ backgroundColor:variant.variantColor }"
-                @mouseover="updateProduct(index)"
-                ></div>
-                <div class="cart">
-                    <p>Cart({{ cart }})</p>
-                </div>
-                <button
-                    v-on:click="addToCart"
-                    :disabled="!inStock"
-                    :class="{ disabledButton: !inStock }"
-                >
-                    Add to cart
-                </button>
-            </div>
+        <div class="product-image">
+        <img :src="image" :alt="altText"/>
+        </div>
+        <div class="product-info">
+        <h1>{{ title }}</h1>
+        <p v-if="inStock">In stock</p>
+        <p v-else>Out of Stock</p>
+        <ul>
+        <li v-for="detail in details">{{ detail }}</li>
+        </ul>
+        <p>Shipping: {{ shipping }}</p>
+        <div
+        class="color-box"
+        v-for="(variant, index) in variants"
+        :key="variant.variantId"
+        :style="{ backgroundColor:variant.variantColor
+        }"
+        @mouseover="updateProduct(index)"
+        ></div>
+        <button
+        v-on:click="addToCart"
+        :disabled="!inStock"
+        :class="{ disabledButton: !inStock }"
+        >
+        Add to cart
+        </button>
+        </div>
         </div>
     `,
     data() {
@@ -41,71 +42,64 @@ Vue.component('product', {
             brand: 'Vue Mastery',
             selectedVariant: 0,
             altText: "A pair of socks",
+            details: ['80% cotton', '20% polyester',
+            'Gender-neutral'],
             variants: [
                 {
                     variantId: 2234,
                     variantColor: 'green',
-                    variantImage: "./assets/vmSocks-green-onWhite.jpg",
+                    variantImage:
+                    "./assets/vmSocks-green-onWhite.jpg",
                     variantQuantity: 10
                 },
                 {
                     variantId: 2235,
                     variantColor: 'blue',
-                    variantImage: "./assets/vmSocks-blue-onWhite.jpg",
+                    variantImage:
+                    "./assets/vmSocks-blue-onWhite.jpg",
                     variantQuantity: 0
                 }
             ],
-            cart: 0
-            }
+        }
     },
     methods: {
         addToCart() {
-            this.cart += 1
-        },
+            this.$emit('add-to-cart', this.variants[this.selectedVariant].variantId);
+        },            
         updateProduct(index) {
             this.selectedVariant = index;
             console.log(index);
         }
     },
     computed: {
-        shipping() {
-            if (this.premium) {
-                return 'Free';
-            } else {
-                return 2.99
-            }
-        },
         title() {
             return this.brand + ' ' + this.product;
         },
         image() {
-            return this.variants[this.selectedVariant].variantImage;
+            return  this.variants[this.selectedVariant].variantImage;
         },
-        inStock(){
+        inStock() {
             return this.variants[this.selectedVariant].variantQuantity
+        },
+        shipping() {
+            if (this.premium) {
+                return "Free";
+            } else {
+                return 2.99
+            }
         }
-    },
-})
-
-Vue.component('product-details', {
-    props: {
-        details: {
-            type: [],
-            required: true,
-        }
-    },
-    template: `
-        <ul>
-            <li v-for="detail in details">{{ detail }}</li>
-        </ul>
-    `
+    }
 })
 
 let app = new Vue({
     el: '#app',
     data: {
         premium: true,
-        details: ['80% cotton', '20% polyester', 'Gender-neutral'],
+        cart: [],
+    },
+    methods: {
+        updateCart(id) {
+            this.cart.push(id);
+        },
     }
 })
-    
