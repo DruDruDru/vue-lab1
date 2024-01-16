ba9f38c15d6a3656705df8ab67a28f4c2034fece
DruDruDru <borisrasgard251@gmail.com>
2024-01-16 20:13:26 +0700

    full-build lab1

--- js/main.js
+++ js/main.js
@@ -30,6 +30,7 @@ Vue.component('product-tabs', {
                         <p>{{ review.name }}</p>
                         <p>Rating: {{ review.rating }}</p>
                         <p>{{ review.review }}</p>
+                        <p>{{ review.recommend }}</p>
                     </li>
                 </ul>
             </div>
@@ -91,6 +92,17 @@ Vue.component('product-review', {
                     </select>
                 </p>
                 <p>
+                    <p>Would you recommend this product?</p>
+                    <p>
+                        <label for="no">No</label>
+                        <input type="radio" id="no" class="radio" name="answer" value="no" v-model="recommend" />
+                    </p>
+                    <p>
+                        <label for="yes">Yes</label>
+                        <input type="radio" id="yes" class="radio" name="answer" value="yes" v-model="recommend" />
+                    </p>
+                </p>
+                <p>
                     <input type="submit" value="Submit">
                 </p>
             </form>
@@ -100,25 +112,29 @@ Vue.component('product-review', {
             name: null,
             review: null,
             rating: null,
+            recommend: null,
             errors: []
         }
     },
     methods: {
         onSubmit() {
-            if (this.name && this.review && this.rating) {
+            if (this.name && this.review && this.rating && this.recommend) {
                 let productReview = {
                     name: this.name,
                     review: this.review,
-                    rating: this.rating
+                    rating: this.rating,
+                    recommend: this.recommend
                 }
                 eventBus.$emit('review-submitted', productReview)
                 this.name = null
                 this.review = null
                 this.rating = null
+                this.recommend = null
             } else {
                 if (!this.name) this.errors.push("Name required.")
-                if (!this.review) this.errors.push("Reviewrequired.")
-                if (!this.rating) this.errors.push("Ratingrequired.")
+                if (!this.review) this.errors.push("Review required.")
+                if (!this.rating) this.errors.push("Rating required.")
+                if (!this.recommend) this.errors.push('Recommend required.')
             }
         }
     }
@@ -137,12 +153,21 @@ Vue.component('product', {
             </div>
             <div class="product-info">
                 <h1>{{ title }}</h1>
+                <p>{{ description }}</p>
+                <a :href="link">More products like this</a>
                 <p v-if="inStock">In stock</p>
-                <p v-else>Out of Stock</p>
+                <p v-else :class="{ outOfStock: !inStock }">Out of Stock</p>
+                <span>{{ sale }}</span>
+                <ul>
+                    <li v-for="size in sizes">{{ size }}</li>
+                </ul>
                 <div class="color-box" v-for="(variant, index) in variants" :key="variant.variantId" :style="{ backgroundColor:variant.variantColor
             }" @mouseover="updateProduct(index)"></div>
-                <button v-on:click="addToCart" :disabled="!inStock" :class="{ disabledButton: !inStock }">
+                <button @click="addToCart" :disabled="!inStock" :class="{ disabledButton: !inStock }">
                     Add to cart
+                </button>
+                <button @click="removeFromCart" :disabled="!inStock" :class="{ disabledButton: !inStock }">
+                    Remove
                 </button>
             </div>
             <product-tabs 
@@ -155,9 +180,13 @@ Vue.component('product', {
     data() {
         return {
             product: "Socks",
+            description: 'A pair of warm, fuzzy socks',
             brand: 'Vue Mastery',
+            link: "https://www.amazon.com/s?k=socks&ref=nb_sb_noss",
             selectedVariant: 0,
             altText: "A pair of socks",
+            onSale: false,
+            sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL'],
             details: ['80% cotton', '20% polyester', 'Gender-neutral'],
             variants: [
                 {
@@ -183,6 +212,10 @@ Vue.component('product', {
             this.$emit('add-to-cart',
                 this.variants[this.selectedVariant].variantId);
         },
+        removeFromCart() {
+            this.$emit('remove-from-cart', 
+                this.variants[this.selectedVariant].variantId);
+        },
         updateProduct(index) {
             this.selectedVariant = index;
             console.log(index);
@@ -204,6 +237,10 @@ Vue.component('product', {
         inStock() {
             return this.variants[this.selectedVariant].variantQuantity
         },
+        sale() {
+            sales = this.onSale ? 'Sale' : '(No sale)'
+            return sales + ' ' + this.product + ' ' +  this.brand ;
+        }
     }
 })
 
@@ -214,8 +251,14 @@ let app = new Vue({
         cart: []
     },
     methods: {
-        updateCart(id) {
-            this.cart.push(id);
+        add(id) {
+            this.cart.push(id)
+            console.log(this.cart);
+        },
+        remove(id) {
+            idx = this.cart.indexOf(id);
+            this.cart = this.cart.filter((value, index) => idx !== index)
+            console.log(this.cart);
         }
     }
 })
